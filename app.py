930ea9d7fa8a24f6b5fff4f4eda4b89493586bcc
Romel Donovan <romeldonovan@live.ca>
2022-10-18 21:34:12 -0400

    account info

--- app.py
+++ app.py
@@ -1,31 +1,14 @@
 # Valorant ask for username
-from wsgiref import headers
-from bs4 import BeautifulSoup
 import requests
 import json
 
-# Use requests and beautiful soup 4 to web scrape tracker.gg
-    # resp = requests.get("https://api.henrikdev.xyz/valorant/v1/account/"+username+'/'+tag)
-    # data = json.loads(resp.text)
-    # print("resp: ", resp.text)
-    # print("json: ", json.loads(resp.text))
-    # print("account level: ", data["data"]["account_level"])
-def retrieve_data():
-    # resp = requests.get("https://tracker.gg/valorant/profile/riot/"+ username +"%23" + tag + "/overview")
-    # resp = requests.get("https://tracker.gg/valorant/profile/riot/Rude%23NA13/overview")
-    headers = {
-        "user-agent":"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/106.0.0.0 Safari/537.36"
-        }
-    resp = requests.get("https://api.tracker.gg/api/v2/valorant/standard/matches/riot/fearflawless%23FEAR?type=competitive", headers=headers)
-    soup = BeautifulSoup(resp.content, 'html.parser')
-    print(soup)
+# Use requests and json to web scrape henrikdev
 
-
-
-    
-
-    # print(soup.prettify()) # print the parsed data of HTML
-
+def retrieve_data(username, tag):
+    resp = requests.get("https://api.henrikdev.xyz/valorant/v2/mmr/na/" + username + '/' + tag)
+    data = json.loads(resp.text)
+    print("Username: ", data["data"]["name"] + "#" + data["data"]["tag"])
+    print("Rank: ", data["data"]["current_data"]["currenttierpatched"] + " RR: ", data["data"]["current_data"]["ranking_in_tier"])
 
 # win/lose, K/D, Headshot%, MMR
 def parse_data():
@@ -36,9 +19,9 @@ def display_data():
     pass
 
 def main():
-    # username = input("Please enter your Valorant username:\n")
-    # tag = input("Please enter your Valorant tag:\n")
-    retrieve_data()
+    username = input("Please enter your Valorant username:\n")
+    tag = input("Please enter your Valorant tag:\n")
+    retrieve_data(username, tag)
     parse_data()
     display_data()
 
